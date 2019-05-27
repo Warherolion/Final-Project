247cc224dc093cc21a756673c2cb4074847f37c3
warherolion <ranvir1412@gmail.com>
2019-05-27 14:14:07 -0400

    Implemented cover image for pygame

--- pyGame stuff/main.py
+++ pyGame stuff/main.py
@@ -1,4 +1,17 @@
 import pygame
 pygame.init()
-win = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Hello")
+
+pygame.display.set_caption("Monopoly Python Version")
+
+screen = pygame.display.set_mode((1000, 600))
+
+cover = pygame.image.load("Images and assets/MONOCOVER.bmp")
+black = pygame.Color(0,0,0)
+gameExit = False
+while not gameExit:
+    for event in pygame.event.get():
+        screen.blit(cover, (0,0))
+        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(324, 385, 354, 35))
+        pygame.display.update()
+pygame.quit
+quit()
