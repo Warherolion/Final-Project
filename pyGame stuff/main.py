d015f7d212c6f7d83cf12c05297e72984c0faa7f
warherolion <ranvir1412@gmail.com>
2019-06-07 22:57:31 -0400

    d

--- pyGame stuff/main.py
+++ pyGame stuff/main.py
@@ -14,7 +14,9 @@ clock = pygame.time.Clock()
 cover = pygame.image.load("Images and assets/MONOCOVER.bmp")
 instruct = pygame.image.load("Images and assets/instruct.bmp")
 settingsSavedImg = pygame.image.load("Images and assets/SavedSettings.bmp")
-
+# Name Asker
+PlayerName2 = pygame.image.load("Images and assets/PlayerName2.bmp")
+PlayerName4 = pygame.image.load("Images and assets/PlayerName4.bmp")
 # Color Definitions 
 black = pygame.Color(0,0,0)
 white = pygame.Color(255, 255, 255)
@@ -60,33 +62,62 @@ def instructPage():
             if event.type == pygame.MOUSEBUTTONDOWN:            
                 if mouseX>((25.15/35.28)*width) and mouseX<((34.64/35.28)*width):
                     if mouseY > ((0.71/23.04)*height) and mouseY <((21.41/23.04) * height):
-                        height = 600
+                        height= 600
                         screen = pygame.display.set_mode((width, height))
                         return 
         pygame.display.update() 
 
+
 def settingsSaved():
+
     settingsCheck = open("settings.txt", "r")
     if "MonoSet1-1" in settingsCheck.read():
         settingsSaved = True
     else:
         settingsSaved = False
     if settingsSaved == True:
-        height = 600
-        screen = pygame.display.set_mode((width, height))
         while True:
+            mouseX, mouseY = pygame.mouse.get_pos()
             screen.blit(settingsSavedImg, ((9.17/35.28)*width,(5.54/23.28)*height))
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:            
-                    if mouseX>((25.15/35.28)*width) and mouseX<((34.64/35.28)*width):
-                        if mouseY > ((0.71/23.04)*height) and mouseY <((21.41/23.04) * height):
-                            height = 600
-                            screen = pygame.display.set_mode((width, height))
-                            return 
+            for event in pygame.event.get():            
+                if event.type == pygame.MOUSEBUTTONDOWN:             
+                    # Checks if the user clicked on the Yes button
+                    if mouseX>((10.48/35.28)*width) and mouseX<((14.04 /35.28)*width):
+                        if mouseY > ((12.88/21.17)*height) and mouseY <((13.62/21.17) * height):
+                            # asks the user for the names based off of the settings
+                            playerName()
+                            pass
+                    # Checks if the user clicks on the No button
+                    elif mouseX>((21.59/35.28)*width) and mouseX<((25.15 /35.28)*width):
+                        if mouseY > ((12.88/21.17)*height) and mouseY <((13.62/21.17) * height):
+                            print("no")
+                            return    
+            
             pygame.display.update() 
     else:
         print(settingsSaved)
-
+def playerName():
+    """
+        players = []
+        for i in range(int(settings[1])):
+                                players.append(
+                                    {
+                                    "playerName": name,
+                                    "money": settings[4],
+                                    "properties": [],
+                                    "Colors": [],
+                                    "railroads": [],
+                                    "inJail":   False,
+                                    "PlayerLocation": 0
+                                    }
+                                )
+    """
+    
+    if int(settings[1]) == 2:
+        screen.blit(cover, (0,0))
+        screen.blit(PlayerName2, (0,0))
+    elif int(settings[1] == 4):
+        screen.blit(PlayerName4, (9.17/35.28)*width,(5.54/23.28)*height)
 # Game Exit Loop
 gameExit = False
 
