84b32a3ca19608c72946ee9d378db986b2643076
warherolion <ranvir1412@gmail.com>
2019-06-09 14:31:39 -0400

    d

--- pyGame stuff/main.py
+++ pyGame stuff/main.py
@@ -17,8 +17,15 @@ instruct = pygame.image.load("Images and assets/instruct.bmp")
 settingsSavedImg = pygame.image.load("Images and assets/SavedSettings.bmp")
 
 
+# Player Pieces
+
+Player1 = pygame.image.load("Images and assets/PlayPiece1.bmp")
+Player2 = pygame.image.load("Images and assets/PlayPiece2.bmp")
+Player3 = pygame.image.load("Images and assets/PlayPiece3.bmp")
+Player4 = pygame.image.load("Images and assets/PlayPiece4.bmp")
+
 #Monopoly Board
-board = pygame.image.load("Images and assets/monopolyBoard.bmp")
+board = pygame.image.load("Images and assets/MonoBoard.bmp")
 # Name Asker
 PlayerName2 = pygame.image.load("Images and assets/PlayerName2.bmp")
 PlayerName4 = pygame.image.load("Images and assets/PlayerName4.bmp")
@@ -30,7 +37,7 @@ color_active = pygame.Color('dodgerblue2')
 color = color_inactive
 
 
-players = []
+
 # File access
 settings = []
 with open("settings.txt") as file:
@@ -56,10 +63,66 @@ if "MonoSet1-1" in settingsCheck.read():
 else:
     settingsSaved = False
 
+players = [                    
+        {
+                    "playerName": "Ranvir",
+                    "money": settings[4],
+                    "properties": ["Yes", "Yeet"],
+                    "Colors": [],
+                    "railroads": [],
+                    "inJail":   False,
+                    "PlayerLocation": 0
+                    }]
+width = 1100
+height = 707
+screen = pygame.display.set_mode((width, height))
+screen.blit(board, (0,0))
+# Checks to see how many people are playing
+if int(settings[1]) == 1:
+    screen.blit(Player1, (640,640))
+elif int(settings[1]) == 2:
+    screen.blit(Player1, (640,640))
+    screen.blit(Player2, (650, 650))
+elif int(settings[1]) == 3:
+    screen.blit(Player1, (640,640))
+    screen.blit(Player2, (650,650))
+    screen.blit(Player3, (670,670))
+elif int(settings[1]) == 4:
+    screen.blit(Player1, (640,640))
+    screen.blit(Player2, (650,650))
+    screen.blit(Player3, (670,670))
+    screen.blit(Player4, (680,680))
 
-def MainGame():
-    pass
+# Creates player information tab on side of board
+if int(settings[1]) == 1:
+    PlayInfor = pygame.Rect(710, 0, 390, 707)
 
+elif int(settings[1]) == 2:
+    subfont = pygame.font.Font("Fonts/MONOPOLY_INLINE.ttf",30)
+    PlayInfor = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    x = 0
+    playerinfo = myfont.render(players[0]["playerName"], False, (0, 0, 0))
+    playerProperties = subfont.render("Owned Properties: ", False, (0, 0, 0))
+    screen.blit(playerinfo, (730, 10))
+    screen.blit (playerProperties, (730, 60))
+    
+    for c in range (len(players[x]["properties"])):
+        playerProp = subfont.render(players[x]["properties"][c], False, (0, 0, 0))
+        propHeight = 100
+        screen.blit (playerProp, (730, propHeight))
+        propHeight += 100
+
+elif int(settings[1]) == 3:
+    PlayInfor = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    PlayInfor2 = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    PlayInfor3 = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+elif int(settings[1]) == 4:
+    PlayInfor = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    PlayInfor2 = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    PlayInfor3 = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+    PlayInfor4 = pygame.draw.rect(screen, white, (710, 0, 390, 707))
+
+"""
 # Instructions Page 
 def instructPage():
     #updates screen to size that fits the instructions page
@@ -161,11 +224,12 @@ while not gameExit:
                 elif mouseY > ((18.27/21.17) * height) and mouseY < ((19.51/21.17) * height):
                     print("Instructions")
                     instructPage()
+    """
+# Updates Screen
+pygame.display.update()
 
-    # Updates Screen
-    pygame.display.update()
-    
-    clock.tick(60)
+clock.tick(60)
+
 pygame.quit
 quit()
 
